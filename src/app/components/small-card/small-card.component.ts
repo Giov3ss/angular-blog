43483245d90a0e677189ea676dec387a0c9d5f48
Giov3ss <giovani.sp25@gmail.com>
2023-09-28 16:49:05 +0100

    final commit, content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+
   @Input()
   photoCover: string = ""
+
   @Input()
   cardTitle: string = ""
+  
+  @Input()
+  Id: string = "0"
 
   constructor() { }
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+    "id": "1", 
+    "title": "Angular vs React",
+    "description": "lorem epsum lorem epsumlorem epsum lorem epsum lorem epsum lorem epsum",
+    "photoCover": "https://www.mobinius.com/wp-content/uploads/2019/04/React-vs-Angular.png"
+    },
+    {
+    "id": "2", 
+    "title": "Bun 1.0 is out of the oven",
+    "description": "Bun, an all-in-one toolkit for building, testing, debugging, and running JavaScript and TypeScript apps, has reached stable production-ready status as of September 8.",
+    "photoCover": "https://uploads-ssl.webflow.com/63ce503ba9b7eb367b63c343/63efb58c52625ef84dd5425d_bunarticleheader.jpg"
+    },
+    {
+    "id": "3", 
+    "title": "What makes Elon Musk tick? I spent months following the same people as him to find out who fuels his curious worldview",
+    "description": "Musk is the hands-on head of three mega-companies, one (Tesla) wildly successful, one (SpaceX) madly aspirational, one (Twitter/X) a shambles. He has plenty of other businesses on the side, including The Boring Company (which makes hi-tech tunnels), Neuralink (which makes brain-computer interfaces), and his current pet favourite xAI (mission: “To understand the true nature of the universe”)",
+    "photoCover": "https://i.guim.co.uk/img/media/9ddd224df2a99140549b0dd88178ced0287b9aea/0_1572_6213_3727/master/6213.jpg?width=880&dpr=1&s=none"
+    },
+    {
+    "id": "4", 
+    "title": "ChatGPT can now access up to date information",
+    "description": "OpenAI, the Microsoft-backed creator of ChatGPT, has confirmed the chatbot can now browse the internet to provide users with current information.The artificial intelligence-powered system was previously trained only using data up to September 2021.The move means some premium users will be able to ask the chatbot questions about current affairs, and access news.OpenAI said the feature would open up to all users soon.",
+    "photoCover": "https://ichef.bbci.co.uk/news/976/cpsprodpb/772B/production/_131270503_337bb1da0f862f76f1d0dfbaa1a8078cec2fce200_201_7511_42271000x563.jpg.webp"
+    }
+]
